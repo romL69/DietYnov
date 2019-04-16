01581d65ce97eef224741df1d0eee78051d726a3
romL69 <romain.loire@ynov.com>
2019-04-16 09:33:34 +0200

    Http Handler + activities

--- app/src/main/java/com/ynov/dietynov/MainActivity.java
+++ app/src/main/java/com/ynov/dietynov/MainActivity.java
@@ -20,22 +20,13 @@ public class MainActivity extends AppCompatActivity
 
     Button weighButton;
     Button sizeButton;
-    Button receipesButton;
+    Button recipesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,8 +43,8 @@ public class MainActivity extends AppCompatActivity
         sizeButton = findViewById(R.id.size);
         sizeButton.setOnClickListener(clickSizesListener);
 
-        receipesButton = findViewById(R.id.receipes);
-        receipesButton.setOnClickListener(clickReceipiesListener);
+        recipesButton = findViewById(R.id.receipes);
+        recipesButton.setOnClickListener(clickRecipiesListener);
     }
 
     @Override
@@ -94,17 +85,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_myaccount) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_recipes) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_home) {
 
         }
 
@@ -127,11 +112,12 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }
     };
-    private View.OnClickListener clickReceipiesListener = new View.OnClickListener() {
+    private View.OnClickListener clickRecipiesListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this, ReceipesActivity.class);
             startActivity(intent);
         }
     };
+
 }
